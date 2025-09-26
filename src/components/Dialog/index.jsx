b416654744becdd6f01c83c401a8ff9683bcd471
Ikejiri-1 <pedroike21@gmail.com>
2025-09-26 12:39:46 -0300

    criação da modal com dialog, e utilizando useRef, useState e useEffect

---
+++ src/components/Dialog/index.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useRef } from "react";
+import "./dialog.style.css";
+import { IconClose } from "../icons";
+
+export function Dialog({ isOpen, onClose, children }) {
+  const dialogRef = useRef(null);
+
+  useEffect(() => {
+    if (isOpen) {
+      openDialog();
+    } else {
+      closeDialog();
+    }
+  }, [isOpen]);
+  function openDialog() {
+    dialogRef.current.showModal();
+  }
+  function closeDialog() {
+    dialogRef.current.close();
+  }
+
+  return (
+    <>
+      <dialog ref={dialogRef} className="dialog">
+        <div className="close-btn-wrapper">
+          <button className="close-btn" autoFocus onClick={onClose}>
+            <IconClose></IconClose>
+          </button>
+        </div>
+        {children}
+      </dialog>
+    </>
+  );
+}
